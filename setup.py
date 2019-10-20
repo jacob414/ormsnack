1df7cc0b050af146234617f3875f296d0e730f2a
Jacob Oscarson <jacob@414soft.com>
2019-10-20 19:10:34 +0200

    Vague skeleton

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# yapf
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+install = (
+    "funcy>=1.10.2",
+    "pysistence>=0.4.1",
+    "patterns>=0.3",
+    "jsonpickle>=1.2",
+)  # yapf: disable
+
+develop = (
+    "pytest>=5.0.1",
+    "hypothesis>=4.24.3",
+    "altered_states>=1.0.9",
+)  # yapf: disable
+
+setup(
+    name='micropy',
+    version='0.0.1',
+    description=next(line for line in open('README.org').readlines()
+                     if line.startswith('* '))[2:],
+    packages=('ormsnack', ),
+    author='Jacob Oscarson',
+    author_email='jacob@414soft.com',
+    install_requires=install,
+    extras_require={
+        'test': install + develop,
+    },
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License (MIT)',
+        'Operating System :: OS Independent',
+    ],
+)
