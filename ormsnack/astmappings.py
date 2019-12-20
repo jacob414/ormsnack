c402f7c643f09ac135606dc49c7fcd7fa5464e0b
Jacob Oscarson <jacob@414soft.com>
2019-12-20 13:05:37 +0100

    Ok.. lots of experiments later, this is probably what I should do

--- ormsnack/astmappings.py
+++ ormsnack/astmappings.py
@@ -3,7 +3,22 @@
 from _ast import *
 import ast
 from micropy.lang import callbytype  # type: ignore
-from typing import Any
+from typing import Any, Iterable
+from dataclasses import dataclass
+
+
+@dataclass
+class NodeDesc(object):
+    spec: Any
+    ident: str
+    value: Any
+
+    @property
+    def children(self) -> Iterable:
+        return []
+
+
+N = NodeDesc
 
 
 def fundef_desc(node: ast.FunctionDef) -> str:
@@ -12,34 +27,58 @@ def fundef_desc(node: ast.FunctionDef) -> str:
                            ', '.join(arg.arg for arg in node.args.args))
 
 
+odesc = callbytype({
+    BinOp:
+    lambda bo: N(spec='bo',
+                 ident='bo',
+                 value=[odesc(bo.left),
+                        odesc(bo.op),
+                        odesc(bo.right)]),
+    Expr:
+    lambda expr: N(spec='ex', ident='ex', value=odesc(expr.value)),
+    Str:
+    lambda s: N(spec=str, ident='str', value=s.s),
+    Num:
+    lambda num: N(spec=type(num.n), ident=str(num.n), value=num.n),
+    Add:
+    lambda a: N(spec='op', ident='+', value='+'),
+    Return:
+    lambda ret: N(spec='return', ident='return', value=odesc(ret.value)),
+    Name:
+    lambda name: N(spec=name.id, ident=name.id, value=name.id)
+})
+
 # Express as code / for human
 desc = callbytype({
     Str:
-    lambda s: (str, "'{}'".format(s.s)),
+    lambda s: (str, "'{}'".format(s.s), ((s.s, ), ())),
     Add:
-    lambda _: ("+", '+'),
+    lambda _: ("+", '+', (('+', ), ())),
     Sub:
-    lambda _: ('-', '-'),
+    lambda _: ('-', '-', (('-'), ())),
     Return:
-    lambda kids: ('return', 'return {}'),
+    lambda kids: ('return', 'return {}', (desc(kids.value)[2], ())),
     FunctionDef:
-    lambda fdef: ('def', fundef_desc(fdef)),
+    lambda fdef: ('def', fundef_desc(fdef), (fdef.body, fdef.args.args)),
     arguments:
     lambda args: ('()', '({})'.format(', '.join(
-        (arg.arg for ar in args.args)))),
+        (arg.arg for ar in args.args),
+        (args.args, ()),
+    ))),
     arg:
-    lambda arg: ('arg', arg.arg),
+    lambda arg: ('arg', arg.arg, ((arg.arg, ), ())),
     Module:
-    lambda mod: ('module', '?'),
+    lambda mod: ('module', '?', (module.body, ())),
     BinOp:
-    lambda bo: ('BO',
-                (desc(bo.left), desc(bo.op), desc(bo.right))),  # type: ignore
+    lambda bo: ('BO', (desc(bo.left), desc(bo.op), desc(bo.right)),
+                (bo.left, bo.op, bo.right), ()),  # type: ignore
     Num:
-    lambda num: (type(num.n), num.n),
+    lambda num: (type(num.n), num.n, ((num.n, ), ())),
     Name:
-    lambda name: ('symbol', name.id),
+    lambda name: ('symbol', name.id, ((name.id, ), ())),
     Expr:
-    lambda exp: ('ex', desc(exp.value)[1]),  # type: ignore
+    lambda exp: ('ex', desc(exp.value)[1], ((exp.value, ),
+                                            ())),  # type: ignore
 })
 
 # Unpack values / condition
@@ -48,10 +87,10 @@ subnodes = callbytype({
     Add: lambda add_: (('+', ), ()),
     Sub: lambda add_: (('-', ), ()),
     Return: lambda ret: subnodes(ret.value),  # type: ignore
-    FunctionDef: lambda fdef: (fdef.body, fdef.args.args),
+    FunctionDef: lambda fdef: (fdef.body, fdef.args),
     arguments: lambda args: (args.args, ()),
     Expr: lambda expr: ((expr.value, ), ()),
-    BinOp: lambda bo: ((bo.left, bo.op, bo.right), ()),
+    BinOp: lambda bo: ((bo.left, bo.yop, bo.right), ()),
     Num: lambda num: ((num.n, ), ()),
     Name: lambda name: ((name.id, ), ()),
     arg: lambda arg_: ((arg_.arg, ), ()),
