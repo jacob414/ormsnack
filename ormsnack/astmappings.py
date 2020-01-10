ba06abeaa25e7f66b2ecb3f71ed923603f553de0
Jacob Oscarson <jacob@414soft.com>
2020-01-10 17:40:07 +0100

    Merged AST mapping re-work
    Squashed commit of the following:
    
    commit 23fc3737b99f15f51e89b79966517362c680657e
    Author: Jacob Oscarson <jacob@414soft.com>
    Date:   Fri Jan 10 17:39:09 2020 +0100
    
        More whitebox testing & bugfixes
    
    commit 3bdb1f6115a3ab260878f7107236328ccbf108c5
    Author: Jacob Oscarson <jacob@414soft.com>
    Date:   Thu Jan 9 16:13:05 2020 +0100
    
        Bugfix + whitebox test of writing to native ast nodes.
    
    commit 22b58ec0554fab2ffc275256ff4afe621d1b36b8
    Author: Jacob Oscarson <jacob@414soft.com>
    Date:   Thu Jan 9 15:43:00 2020 +0100
    
        Implemented a 'Whitebox' test of planned new functionallity of node mapping.
    
    commit e193dd540a7b8c21311965272686931a8f2d0e04
    Author: Jacob Oscarson <jacob@414soft.com>
    Date:   Thu Jan 9 12:02:29 2020 +0100
    
        Refactored AST node mapping for flexibility and writeability

--- ormsnack/astmappings.py
+++ ormsnack/astmappings.py
@@ -3,9 +3,27 @@
 from _ast import *
 import ast
 from micropy.lang import callbytype  # type: ignore
-from typing import Any, Iterable
+from typing import Any, Iterable, Callable
 from dataclasses import dataclass
 import funcy
+
+
+def attrsetter(attrname: str) -> None:
+    "Does attrsetter"
+
+    def set_and_return_self(node: ast.AST, value: Any) -> Any:
+        setattr(node.full, attrname, value)
+
+    return set_and_return_self
+
+
+def attrgetter(attrname: str) -> Callable[[ast.AST], Any]:
+    "Does attrsetter"
+
+    def get_node_value(node: ast.AST) -> Any:
+        return getattr(node, attrname)
+
+    return get_node_value
 
 
 @dataclass
@@ -13,56 +31,95 @@ class NodeDesc(object):
     full: ast.AST
     spec: Any
     ident: str
-    value: Any
+    get: Callable[[], 'NodeDesc']
+    set: Callable[[Any], None]
     cond: Any = None
 
     @property
     def children(self) -> Iterable:
-        if funcy.is_seqcoll(self.value):
-            return self.value
+        value = self.value
+        if funcy.is_seqcoll(value):
+            return value
         else:
             return []
 
-    def __hash__(self):
-        return hash(self.full) + hash(self.value) + hash(self.spec) + hash(
-            self.ident)
+    @property
+    def value(self) -> Any:
+        return self.get(self.full)
+
+    @value.setter
+    def value(self, value) -> 'NodeDesc':
+        "Does _"
+        self.set(self, value)
+
+    def __len__(self) -> int:
+        "Does __len__"
+        children = self.children
+        if funcy.is_seqcoll(children):
+            return len(children)
+        else:
+            raise
+        return len(self.children)
+
+    def __getitem__(self, idx) -> Any:
+        return self.children[idx]
 
 
 N = NodeDesc
 
 
-def desc_many(nodes: Iterable[ast.AST]) -> Iterable[NodeDesc]:
+def decender(nodes: Iterable[ast.AST]) -> Callable[[], Iterable[NodeDesc]]:
     "Creates NodeDesc objects from an iterable of ast nodes."
-    return [desc(node) for node in nodes]
+
+    def decend(_: ast.AST) -> Iterable[NodeDesc]:
+        print(nodes)
+        return [desc(node) for node in nodes]
+
+    return decend
+
+
+def fwd_get(attrname: str) -> N:
+    "Returns a composition of `desc()` through `gettattr`"
+
+    def desc_and_get(node: ast.AST) -> N:
+        "Does desc_and_get"
+        res = desc(getattr(node, attrname))
+        return res
+
+    return desc_and_get
 
 
 desc = callbytype({  # hm? starting out by just descending into it..
     BinOp:
-    lambda bo: desc_many([bo.left, bo.op, bo.right]),
+    lambda bo: decender([bo.left, bo.op, bo.right]),
     Expr:
     lambda expr: desc(expr.value),
     If:
     lambda iff: N(full=iff,
                   spec='if',
                   ident='if',
-                  value=desc_many(iff.body),
+                  get=attrgetter('body'),
+                  set=attrsetter('body'),
                   cond=desc(iff.test)),
     Call:
     lambda call: N(full=call,
                    spec=f'call/{call.func.id}',
                    ident=call.func.id,
-                   value=desc(call.func)),
+                   get=attrgetter('func'),
+                   set=attrsetter('func')),
     FunctionDef:
     lambda fdef: N(full=fdef,
                    spec=f'def',
                    ident=fdef.name,
-                   value=desc_many(fdef.body),
+                   get=decender(fdef.body),
+                   set=attrsetter('body'),
                    cond=desc(fdef.args)),
     arguments:
     lambda args: N(full=args,
                    spec='args',
                    ident='({})'.format(','.join(arg.arg for arg in args.args)),
-                   value=desc_many(args.args)),
+                   get=decender(args.args),
+                   set=lambda v: setattr(args, 'args', v)),
     arg:
     lambda arg: N(
         full=arg,
@@ -74,16 +131,33 @@ desc = callbytype({  # hm? starting out by just descending into it..
     lambda cmp_: N(full=cmp_,
                    spec='cmp',
                    ident='cmp',
-                   value=desc_many([cmp_.left, *cmp_.comparators])),
+                   get=decender([cmp_.left, *cmp_.comparators]),
+                   set=attrsetter('comparators')),
     Str:
-    lambda s: N(full=s, spec=str, ident=s.s, value=s.s),
+    lambda s:
+    N(full=s, spec=str, ident=s.s, get=attrgetter('s'), set=attrsetter('s')),
     Num:
-    lambda num: N(full=num, spec=type(num.n), ident=str(num.n), value=num.n),
+    lambda num: N(full=num,
+                  spec=type(num.n),
+                  ident=str(num.n),
+                  get=attrgetter('n'),
+                  set=attrsetter('n')),
     Add:
-    lambda a: N(full=a, spec='op', ident='+', value='+'),
+    lambda a: N(full=a,
+                spec='op',
+                ident='+',
+                get=lambda: '+',
+                set=lambda v: setattr(a, 'op', v)),
     Return:
-    lambda ret:
-    N(full=ret, spec='return', ident='return', value=desc(ret.value)),
+    lambda ret: N(full=ret,
+                  spec='return',
+                  ident='return',
+                  get=lambda _: desc(ret.value)(ret.value),
+                  set=lambda v: setattr(ret, 'value', v)),
     Name:
-    lambda name: N(full=name, spec=name.id, ident=name.id, value=name.id)
+    lambda name: N(full=name,
+                   spec=name.id,
+                   ident=name.id,
+                   get=lambda: name.id,
+                   set=lambda v: setattr(name, 'id', v))
 })
---
+++ ormsnack/test/test_wbox_mappings.py
@@ -0,0 +1,64 @@
+# yapf
+
+import pytest
+from typing import Any
+from ormsnack.tree import getast
+from ormsnack import astmappings as M
+from micropy.testing import fixture
+from micropy.dig import dig
+
+
+def foo(x: Any) -> None:
+    "Docstring"
+    1
+    2
+    return x + 3
+
+
+tree = getast(foo)
+
+
+def descnodes() -> list:
+    "Does descnodes"
+    return M.desc(tree.body[0])
+
+
+@pytest.fixture
+def node_types() -> list:
+    "Does nodes"
+    return [type(n.value) for n in descnodes()]
+
+
+@pytest.fixture
+def nodes() -> list:
+    "Does nodes"
+    return descnodes().children
+
+
+@pytest.mark.wbox
+@fixture.params(
+    "index, type_",
+    (0, str),
+    (1, int),
+    (2, int),
+    (3, list),
+)
+def test_typecheck_values(node_types, index, type_) -> None:
+    "Should typecheck_values"
+    assert node_types[index] == type_
+
+
+@pytest.mark.wbox
+@fixture.params(
+    "index, dpath, change",
+    (0, "full.s", "Other string"),
+    (1, "full.n", 1),
+    (2, "full.n", 2),
+    # XXX: not ready to update ast.Return.value yet.
+)
+def test_change_node_value(nodes, index, dpath, change) -> None:
+    "Should change_node_value"
+    node = nodes[index]
+    node.value = change
+    assert node.value == change
+    assert dig(node, dpath) == change
